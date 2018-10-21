d92db9d43bd0f4d4ea0ca8a59a2f55102ba680b3
Dave <Dave.Troupe@gmail.com>
2018-10-21 14:22:10 -0400

    cleaned up

--- PartakeChallenge/Extensions/UIImage+Extension.swift
+++ PartakeChallenge/Extensions/UIImage+Extension.swift
@@ -10,73 +10,25 @@ import UIKit
 
 extension UIImage {
     
-    func scaleImage(toSize size: CGSize) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
-        self.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+    func resizedImageWith(targetSize: CGSize) -> UIImage? {
+        
+        let imageSize = self.size
+        let newWidth  = targetSize.width/self.size.width
+        let newHeight = targetSize.height/self.size.height
+        var newSize: CGSize
+        
+        if newWidth > newHeight {
+            newSize = CGSize(width: imageSize.width * newHeight, height: imageSize.height * newHeight)
+        } else {
+            newSize = CGSize(width: imageSize.width * newWidth, height: imageSize.height * newWidth)
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        self.draw(in: rect)
+        
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return newImage
-    }
-    
-//    func imageWithImage(scaledToMaxWidth: CGFloat, scaledToMaxHeight: CGFloat) {
-//        let oldWidth = self.size.width
-//        let oldHeight = self.size.height
-//        
-//        let scaleFactor = (oldWidth > oldHeight) ? wi
-//    }
-//    
-//    
-//    
-//    + (UIImage *)imageWithImage:(UIImage *)image scaledToMaxWidth:(CGFloat)width maxHeight:(CGFloat)height {
-//    CGFloat oldWidth = image.size.width;
-//    CGFloat oldHeight = image.size.height;
-//    
-//    CGFloat scaleFactor = (oldWidth > oldHeight) ? width / oldWidth : height / oldHeight;
-//    
-//    CGFloat newHeight = oldHeight * scaleFactor;
-//    CGFloat newWidth = oldWidth * scaleFactor;
-//    CGSize newSize = CGSizeMake(newWidth, newHeight);
-//    
-//    return [ImageUtilities imageWithImage:image scaledToSize:newSize];
-//    }
-    
-    func resizeImage(targetSize: CGSize) -> UIImage {
-        let size = self.size
-        let widthRatio  = targetSize.width  / size.width
-        let heightRatio = targetSize.height / size.height
-        
-        let newSize = widthRatio > heightRatio ?  CGSize(width: size.width * heightRatio, height: size.height * heightRatio) : CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
-        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-        
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-        self.draw(in: rect)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage!
-    }
-    
-    public func resize(height: CGFloat) -> UIImage? {
-        let scale = height / self.size.height
-        let width = self.size.width * scale
-        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
-        self.draw(in: CGRect(x:0, y:0, width:width, height:height))
-        let resultImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return resultImage
-    }
-    
-    func resize(width: CGFloat) -> UIImage {
-        let oldWidth = self.size.width
-        let scaleFactor = width / oldWidth
-        
-        let newHeight = self.size.height * scaleFactor
-        let newWidth = oldWidth * scaleFactor
-        
-        UIGraphicsBeginImageContext(CGSize(width:newWidth, height:newHeight))
-        self.draw(in: CGRect(x:0, y:0, width:newWidth, height:newHeight))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage!
     }
 }
--- PartakeChallenge/Views/VenueCell.swift
+++ PartakeChallenge/Views/VenueCell.swift
@@ -55,71 +55,15 @@ class VenueCell: UITableViewCell {
                 if error != nil {
                     print("Error: \(error!.localizedDescription)")
                 } else if let img = image, let sself = self {
-                    
-//                    let ratio = img.size.width / img.size.height
-//                    let newHeight = sself.venueImageView.frame.width / ratio
-//                    sself.imageViewHeightContraint?.constant = newHeight
-//                    sself.layoutIfNeeded()
-                    
-//                    let ratio = img.size.width / img.size.height
-//                    let imageViewRatio = sself.venueImageView.frame.size.width / sself.venueImageView.frame.size.height
-//                    print("imageRatio: \(ratio)")
-//                    print("imageViewRatio: \(imageViewRatio)")
-//
-//                    if sself.containerView.frame.width > sself.containerView.frame.height {
-//                        let newHeight = sself.containerView.frame.width / ratio
-//                        sself.venueImageView.frame.size = CGSize(width: sself.containerView.frame.width, height: newHeight)
-//                    }
-//                    else {
-//                        let newWidth = sself.containerView.frame.height * ratio
-//                        sself.venueImageView.frame.size = CGSize(width: newWidth, height: sself.containerView.frame.height)
-//                    }
-                    
-//                    let resized = img.resize(height: sself.venueImageView.frame.height)
-//                    print("resized.height: \(String(describing: resized?.size.height))")
-//                    print("venueImageView.frame.height: \(sself.venueImageView.frame.height)")
-//                    print("\n")
-//                    sself.venueImageView.image = resized
-                    
-//                    let resized = img.resize(width: sself.venueImageView.frame.width)
-//                    sself.venueImageView.image = resized
-//                    print("resized.width: \(String(describing: resized.size.width))")
-//                    print("resized.height: \(String(describing: resized.size.height))")
-//                    print("venueImageView.frame.height: \(sself.venueImageView.frame.width)")
-//                    print("\n")
-                    
-//                    let resized = img.resizeImage(targetSize: sself.venueImageView.frame.size)
-//                    sself.venueImageView.image = resized
-//                    print("resized.width: \(String(describing: resized.size.width))")
-//                    print("resized.height: \(String(describing: resized.size.height))")
-//                    print("venueImageView.frame.width: \(sself.venueImageView.frame.width)")
-//                    print("venueImageView.frame.height: \(sself.venueImageView.frame.height)")
-//                    print("\n")
-                    
-//                    let resized = sself.resizedImageWith(image: img, targetSize: sself.venueImageView.frame.size)
-//                    sself.venueImageView.image = resized
-//                    print("original.width: \(String(describing: img.size.width))")
-//                    print("original.height: \(String(describing: img.size.height))")
-//                    print("resized.width: \(String(describing: resized?.size.width))")
-//                    print("resized.height: \(String(describing: resized?.size.height))")
-//                    print("\n")
-                    
-                    let cropped = sself.cropToBounds(image: img, width: Double(sself.venueImageView.frame.width), height: Double(sself.venueImageView.frame.height))
-                    sself.venueImageView.image = cropped
-                    print("original.width: \(String(describing: img.size.width))")
-                    print("original.height: \(String(describing: img.size.height))")
-                    print("resized.width: \(String(describing: cropped.size.width))")
-                    print("resized.height: \(String(describing: cropped.size.height))")
-                    print("\n")
-
-
+                    let resized = img.resizedImageWith(targetSize: sself.venueImageView.frame.size)
+                    // let resized = sself.resizedImageWith(image: img, targetSize: sself.venueImageView.frame.size)
+                    sself.venueImageView.image = resized
                 }
             })
         } else {
             // use some default image
         }
-
-        // venueImageView.kf.setImage(with: URL(string: venue.imageURL))
+        
         nameLabel.heightAnchor == 24
         nameLabel.attributedText = venue.name.styled(with: nameStyle)
         nameLabel.numberOfLines = 1
@@ -140,62 +84,6 @@ class VenueCell: UITableViewCell {
         labelStackView.addArrangedSubviews([nameLabel, locationLabel])
         
         selectionStyle = .none
-    }
-    
-    func resizedImageWith(image: UIImage, targetSize: CGSize) -> UIImage? {
-        
-        let imageSize = image.size
-        let newWidth  = targetSize.width/image.size.width
-        let newHeight = targetSize.height/image.size.height
-        var newSize: CGSize
-        
-        if newWidth > newHeight {
-            newSize = CGSize(width: imageSize.width * newHeight, height: imageSize.height * newHeight)
-        } else {
-            newSize = CGSize(width: imageSize.width * newWidth, height: imageSize.height * newWidth)
-        }
-        
-        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-        image.draw(in: rect)
-        
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-    
-    func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
-        
-        let cgimage = image.cgImage!
-        let contextImage: UIImage = UIImage(cgImage: cgimage)
-        let contextSize: CGSize = contextImage.size
-        var posX: CGFloat = 0.0
-        var posY: CGFloat = 0.0
-        var cgwidth: CGFloat = CGFloat(width)
-        var cgheight: CGFloat = CGFloat(height)
-        
-        // See what size is longer and create the center off of that
-        if contextSize.width > contextSize.height {
-            posX = ((contextSize.width - contextSize.height) / 2)
-            posY = 0
-            cgwidth = contextSize.height
-            cgheight = contextSize.height
-        } else {
-            posX = 0
-            posY = ((contextSize.height - contextSize.width) / 2)
-            cgwidth = contextSize.width
-            cgheight = contextSize.width
-        }
-        
-        let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
-        
-        // Create bitmap image from context using the rect
-        let imageRef: CGImage = cgimage.cropping(to: rect)!
-        
-        // Create a new image based on the imageRef and rotate back to the original orientation
-        let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
-        
-        return image
     }
     
     override func prepareForReuse() {
